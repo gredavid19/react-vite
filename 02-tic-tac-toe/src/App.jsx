69e18a9088f79926bd1c9f44e0a01e699d99dd0a
gredavid19 <greuddydlp16@gmail.com>
2025-03-02 18:14:24 -0400

    Tic=tac-toe final

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -5,11 +5,21 @@ import { TURNS } from "./constants.js";
 import { checkWinner, checkEndGame } from "./logic/board.js";
 import './index.css'
 import { WinnerModal } from './components/WinnerModal.jsx';
+import { Board } from './components/Board.jsx';
+import { saveGameToStorage, resetGameStorage } from './storage/index.js';
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  // get item from local storage if exists
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   // null es que no hay ganador, false es empate
   const [winner, setWinner] = useState(null)
 
@@ -23,6 +33,12 @@ function App() {
     //Cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    
+    saveGameToStorage({
+      board: newBoard, 
+      turn: newTurn
+    })
+
     // Verificar si hay un ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
@@ -37,29 +53,17 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Resetar el juego</button>
-      <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square 
-                key={index} 
-                index={index} 
-                updateBoard={updateBoard}
-                checkWinner={checkWinner}
-              >
-                {square}
-              </Square>
-              
-            )
-          })
-        }
-      </section>
+
+      <Board board={board} updateBoard={updateBoard} checkWinner={checkWinner} />
+
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
--- 02-tic-tac-toe/src/components/Board.jsx
+++ 02-tic-tac-toe/src/components/Board.jsx
@@ -1,28 +1,26 @@
 import { Square } from "./Square"
 
-export function Board (board, updateBoard, checkWinner) {
-    const [board, setBoard] = useState(Array(9).fill(null))
+export function Board ({board, updateBoard, checkWinner}) {
+    const newBoard = [...board]
     
-    board.map((square, index) => {
-        return (
-            <section className='game'>
-                {
-                    board.map((square, index) => {
-                    return (
-                        <Square 
-                        key={index} 
-                        index={index} 
-                        updateBoard={updateBoard}
-                        checkWinner={checkWinner}
-                        >
-                        {square}
-                        </Square>
-                        
-                    )
-                    })
-                }
-            </section>
-        
-        )
-    })
+    return (
+        <section className='game'>
+            {
+            newBoard.map((square, index) => {
+                return (
+                <Square 
+                    key={index} 
+                    index={index} 
+                    updateBoard={updateBoard}
+                    checkWinner={checkWinner}
+                >
+                    {square}
+                </Square>
+                
+                )
+            })
+            }
+        </section>
+    
+    )
 }
